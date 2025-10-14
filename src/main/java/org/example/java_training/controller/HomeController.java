253ae06771b6c268e075486ee4b6033200434e68
nguyenhn.nta <nguyenhn.nta@gmail.com>
2025-10-14 16:53:46 +0700

    add home controller

---
+++ src/main/java/org/example/java_training/controller/HomeController.java
@@ -0,0 +1,16 @@
+package org.example.java_training.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("author", "NguyenHN");
+        model.addAttribute("message", "OK");
+        return "home"; // Spring Boot sẽ tìm file templates/home.html
+    }
+}
